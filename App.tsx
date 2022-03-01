e5ae8928334bb717b06d3288dac716c741a1fa2f
Danilo Ferreira <danilo1705@gmail.com>
2022-03-01 10:33:36 -0300

    feature: dogs list prototype added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Image,
+  useWindowDimensions,
+} from 'react-native';
+
+import * as DogsService from './src/services/DogsService';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,13 +18,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '25%',
+    height: 128,
+  },
 });
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [dogs, setDogs] = useState<DogsService.Dog[]>([]);
+
+  const { width } = useWindowDimensions();
+  const widthQuarter = width * 0.25;
+
+  useEffect(() => {
+    async function getInitialImages() {
+      setLoading(true);
+
+      const downloadedDogs = await DogsService.getRandomDogs();
+
+      setDogs(downloadedDogs);
+
+      setLoading(false);
+    }
+
+    getInitialImages();
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator color="#000" size={32} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <FlatList
+        data={dogs}
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: item.uri }}
+            style={{ width: widthQuarter, height: widthQuarter }}
+          />
+        )}
+        keyExtractor={({ id }) => id}
+        numColumns={4}
+      />
+    </SafeAreaView>
   );
 }
---
+++ src/services/DogsService.ts
@@ -0,0 +1,19 @@
+import uuid from 'react-native-uuid';
+
+import { api } from './api';
+
+export interface Dog {
+  id: string;
+  uri: string;
+}
+
+export async function getRandomDogs(): Promise<Dog[]> {
+  const response = await api.get('breeds/image/random/50');
+
+  const dogs: Dog[] = response.data.message?.map((url: string) => ({
+    id: uuid.v4(),
+    uri: url,
+  }));
+
+  return dogs;
+}
