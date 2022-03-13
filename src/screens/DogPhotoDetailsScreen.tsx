a19b1b25412267b55c12f107ad98b30ce630fcf3
Danilo Ferreira <danilo1705@gmail.com>
2022-03-13 10:24:04 -0300

    backup: save changes

--- src/screens/DogPhotoDetailsScreen.tsx
+++ src/screens/DogPhotoDetailsScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 import { AppHeader } from '../components/AppHeader';
 import { SafeAreaView } from '../components/SafeAreaView';
 import { DogPhotoDetailsScreenRouteProps } from '../global/routes/app';
@@ -7,10 +9,18 @@ import { DogPhotoDetailsScreenRouteProps } from '../global/routes/app';
 interface FileInfo {
   breed: string;
   fileName: string;
+  url: string;
 }
 
-export const DogPhotoDetailsScreen: React.FC<DogPhotoDetailsScreenRouteProps> = ({ route }) => {
+export const DogPhotoDetailsScreen: React.FC<DogPhotoDetailsScreenRouteProps> = ({
+  route,
+  navigation,
+}) => {
   const [fileInfo, setFileInfo] = useState<FileInfo>({} as FileInfo);
+
+  const handleGoBack = () => {
+    navigation.pop();
+  };
 
   useEffect(() => {
     const splitUrl = route.params.dog.uri.split('/');
@@ -19,16 +29,59 @@ export const DogPhotoDetailsScreen: React.FC<DogPhotoDetailsScreenRouteProps> = 
 
     const breed = splitUrl[breedsLabelIndex + 1];
     const fileName = splitUrl[splitUrl.length - 1];
+    const url = route.params.dog.uri;
 
-    setFileInfo({ breed, fileName });
+    setFileInfo({ breed, fileName, url });
   }, [route.params.dog.uri]);
 
   return (
     <SafeAreaView>
       <AppHeader />
-      <Text>Hello world!</Text>
-      <Text>{fileInfo.breed}</Text>
-      <Text>{fileInfo.fileName}</Text>
+
+      <View style={{ flex: 1, padding: RFValue(24) }}>
+        <Image
+          source={{ uri: fileInfo.url }}
+          style={{
+            width: '100%',
+            height: RFPercentage(40),
+            borderRadius: RFValue(4),
+            resizeMode: 'cover',
+          }}
+        />
+
+        <View style={{ flex: 1, justifyContent: 'flex-end', paddingVertical: RFValue(32) }}>
+          <Text>Breed:</Text>
+          <Text>{fileInfo.breed}</Text>
+
+          <Text>File name:</Text>
+          <Text>{fileInfo.fileName}</Text>
+
+          <TouchableOpacity
+            onPress={handleGoBack}
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              backgroundColor: '#f39c12',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: RFValue(8),
+              padding: RFValue(8),
+              marginTop: RFValue(16),
+            }}
+          >
+            <Feather name="arrow-left" color="#2c2c2c" size={RFValue(20)} />
+            <Text
+              style={{
+                fontSize: RFValue(16),
+                fontWeight: 'bold',
+                marginLeft: RFValue(8),
+              }}
+            >
+              Go Back
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
