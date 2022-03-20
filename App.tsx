ba1c1ce732a5b7b2d30e42cb24fd7ea451f20543
Danilo Ferreira <danilo1705@gmail.com>
2022-03-20 19:11:37 -0300

    chore: add status bar color

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import { Routes } from './src/routes/routes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="#f4d35e" style="dark" />
       <Routes />
     </NavigationContainer>
   );
